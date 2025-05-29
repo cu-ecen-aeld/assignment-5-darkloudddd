dcc6f7516f8cccac609c1518166f452024e0904e
darkloudddd <victorwu0509@gmail.com>
2025-05-29 20:39:35 +0800

    Remove Config.in include from external.mk

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,6 +1,4 @@
 # base_external/external.mk
-# Pull in our external’s metadata and package makefile
-
 include $(BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH)/external.desc
 include $(BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH)/package/aesd-assignments/aesd-assignments.mk
 
