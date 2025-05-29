e62e49fc4cf6c8bd15da00bf87fc74d2e416d91d
darkloudddd <victorwu0509@gmail.com>
2025-05-29 20:30:11 +0800

    Use BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH in external.mk

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,4 +1,2 @@
-# external.mk — bring in our external packages
-include $(BR2_EXTERNAL_PROJECT_BASE_PATH)/external.desc
-include $(BR2_EXTERNAL_PROJECT_BASE_PATH)/Config.in
-
+include $(BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH)/external.desc
+include $(BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH)/Config.in
