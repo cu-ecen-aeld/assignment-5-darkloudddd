d13f5594e06e1ab33e067202c670bb4c8dd8b09b
darkloudddd <victorwu0509@gmail.com>
2025-05-29 20:32:52 +0800

    Fix external.mk: only include .desc and package .mk

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,2 +1,6 @@
+# base_external/external.mk
+# Pull in our external’s metadata and package makefile
+
 include $(BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH)/external.desc
-include $(BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH)/Config.in
+include $(BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH)/package/aesd-assignments/aesd-assignments.mk
+
