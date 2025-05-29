666ab9b393c94b995e1c5dde7bfb7c9b542d56df
darkloudddd <victorwu0509@gmail.com>
2025-05-29 19:37:15 +0800

    Correct external.mk include paths (remove quotes)

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,4 +1,6 @@
 # base_external/external.mk
+
+# Include our external description and Kconfig
 include $(BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH)/external.desc
 include $(BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH)/Config.in
 
