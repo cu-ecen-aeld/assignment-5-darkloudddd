6cc14f9049ae830931d188d8b7eec48439ae4e2a
darkloudddd <victorwu0509@gmail.com>
2025-05-29 20:55:23 +0800

    Enable BR2_EXTERNAL, fix external.desc & external.mk

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,8 +1,3 @@
-#
-# base_external/external.mk
-# Include our external metadata and package Makefile
-#
-
 include $(BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH)/external.desc
 include $(BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH)/package/aesd-assignments/aesd-assignments.mk
 
