5afea3aa37b91f0fb3abee9f4ff9b88629ebd7b5
darkloudddd <victorwu0509@gmail.com>
2025-06-01 06:48:46 +0800

    Update configs and add clean.sh

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,3 +1,17 @@
+# =============================================================================
+# base_external/external.mk
+#
+# This Makefile fragment is loaded by Buildroot as soon as
+# BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH is defined in .config.
+#
+# It must:
+#   1) include external.desc   (for name/desc/license)
+#   2) include <your-package>.mk  (for rules to build/install)
+# =============================================================================
+
+# 1) Pull in the “name/desc/license” metadata from external.desc
 include $(BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH)/external.desc
+
+# 2) Include our aesd-assignments package .mk
 include $(BR2_EXTERNAL_AESD_ASSIGNMENTS_PATH)/package/aesd-assignments/aesd-assignments.mk
 
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,23 +1,49 @@
-AESD_ASSIGNMENTS_VERSION = master
-AESD_ASSIGNMENTS_SITE = git@github.com:darkloudddd/assignment-3-darkloudddd.git
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_DEPENDENCIES = host-ssh
+#------------------------------------------------------------------------------
+#  aesd-assignments package for Buildroot
+#
+#  We fetch from the "main" branch of your Assignment-3 repo, then
+#  run “make” inside the finder-app/ subdirectory, and install everything
+#  into /usr/bin and /etc/finder-app/conf on the target rootfs.
+#------------------------------------------------------------------------------
 
+AESD_ASSIGNMENTS_VERSION       = main
+AESD_ASSIGNMENTS_SITE          = git@github.com:cu-ecen-aeld/assignments-3-and-later-darkloudddd.git
+AESD_ASSIGNMENTS_SITE_METHOD   = git
+# (No host-ssh dependency needed; Buildroot will use whatever SSH key is already set up
+#  because you successfully cloned over git@github.com above.)
+
+# Build commands: cd into finder-app/ and run “make”
 define AESD_ASSIGNMENTS_BUILD_CMDS
-    $(MAKE) -C $(@D) CC="$(TARGET_CC)" CROSS_COMPILE="$(BR2_TARGET_ARCH)-" all
+	# Build both writer and finder inside finder-app/
+	$(MAKE) -C $(@D)/finder-app \
+	       CC="$(TARGET_CC)" \
+	       CROSS_COMPILE="$(BR2_TARGET_ARCH)-" \
+	       all
 endef
 
+# Install commands: copy the binaries out of finder-app/ into /usr/bin,
+# then copy the conf directory into /etc/finder-app/conf.
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-    # Install the cross-built writer and finder binaries
-    install -D -m 755 $(@D)/writer   $(TARGET_DIR)/usr/bin/writer
-    install -D -m 755 $(@D)/finder   $(TARGET_DIR)/usr/bin/finder
+    # 1) Create /usr/bin on the target
+    $(INSTALL) -d $(TARGET_DIR)/usr/bin
 
-    # Install your test script
-    install -D -m 755 $(@D)/tester.sh $(TARGET_DIR)/usr/bin/finder-test.sh
+    # 2) Copy writer binary
+    $(INSTALL) -D -m 0755 $(@D)/finder-app/writer \
+                   $(TARGET_DIR)/usr/bin/writer
 
-    # Copy the conf directory
-    mkdir -p $(TARGET_DIR)/etc/finder-app/conf
-    cp -r $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+    # 3) Copy the finder.sh script as “finder”
+    $(INSTALL) -D -m 0755 $(@D)/finder-app/finder.sh \
+                   $(TARGET_DIR)/usr/bin/finder
+
+    # 4) Copy finder-test.sh into /usr/bin
+    $(INSTALL) -D -m 0755 $(@D)/finder-app/finder-test.sh \
+                   $(TARGET_DIR)/usr/bin/finder-test.sh
+
+    # 5) Create and populate /etc/finder-app/conf
+    #$(INSTALL) -d $(TARGET_DIR)/etc/finder-app/conf
+    #$(INSTALL) -m 0644 $(@D)/finder-app/conf/finder.conf \
+    #               $(TARGET_DIR)/etc/finder-app/conf/finder.conf
+    # (Copy any other conf files from finder-app/conf/ if needed)
 endef
 
 $(eval $(generic-package))
