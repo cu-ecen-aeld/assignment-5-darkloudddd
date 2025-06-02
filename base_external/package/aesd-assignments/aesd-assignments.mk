a4674aa2d9cf417d9f4790258f259e0036f48dc2
darkloudddd <victorwu0509@gmail.com>
2025-06-03 03:26:19 +0800

    Buildroot: install aesdsocket binary & S99aesdsocket init script

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -27,11 +27,10 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
 	
-	$(INSTALL) -D -m 0755 $(PKG_BUILD_DIR)/server/aesdsocket \
-        	$(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
         	
-        $(INSTALL) -D -m 0755 $(PKG_BUILD_DIR)/server/aesdsocket-start-stop \
-        	$(TARGET_DIR)/etc/init.d/S99aesdsocket
+        $(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop \
+		$(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
